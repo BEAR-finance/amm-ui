00a8e0ce26db1fe7e264a7d5c12af79151b77a18
mindfulme <go.go.gg.rostislav@gmail.com>
2021-02-28 17:50:37 +0200

    init

---
+++ src/constants/bearPriceContracts.ts
@@ -0,0 +1,7 @@
+const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
+  cakeAddress: '0x580f500cc7da45b7b058de7df325f6d8f83065e1',
+  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+  lpAddress: '0x4e4820e5a4e7e7b8f54df4b23fd6a89b57ddf0bb'
+}
+
+export default priceContracts
